e131e29d01a5d6efaf0348b273e6537336dbe91c
VishnuVardhanSaiLanka <vardhanvishnu691@gmail.com>
2025-05-11 23:23:24 -0400

    fixes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,19 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  const defaultAuthor = allAuthors.find((p) => p.slug === 'default') as Authors
+  const sparrowhawkAuthor = allAuthors.find((p) => p.slug === 'sparrowhawk') as Authors
+  const defaultContent = coreContent(defaultAuthor)
+  const sparrowhawkContent = coreContent(sparrowhawkAuthor)
 
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
+    <div className="space-y-8">
+      <AuthorLayout content={defaultContent}>
+        <MDXLayoutRenderer code={defaultAuthor.body.code} />
       </AuthorLayout>
-    </>
+      <AuthorLayout content={sparrowhawkContent}>
+        <MDXLayoutRenderer code={sparrowhawkAuthor.body.code} />
+      </AuthorLayout>
+    </div>
   )
 }
